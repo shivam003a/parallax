992e456fd3827e60c827ea8bddaa48e141f73107
shivam003a <shivam003a@gmail.com>
2023-02-01 18:47:32 +0530

    initial commit

---
+++ src/components/AboutMe.js
@@ -0,0 +1,14 @@
+const AboutMe = () => {
+    return (
+        <>
+            <div className="skills">
+                <h2>Skills</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate odio ut enim blandit. Tempus urna et pharetra pharetra massa. Accumsan lacus vel facilisis volutpat. Ac tincidunt vitae semper quis lectus nulla at volutpat. Aliquam etiam erat velit scelerisque in dictum non consectetur a. Quis lectus nulla at volutpat diam. Viverra accumsan in nisl nisi. Tincidunt arcu non sodales neque sodales ut. Adipiscing at in tellus integer feugiat scelerisque ius morbi.</p>
+
+                <p>Faucibus et molestie ac feugiat sed. Nulla facilisi morbi tempus iaculis urna id volutpat lacus. Sem integer vitae justo eget magna fermentum iaculis eu non. Amet volutpat consequat mauris nunc. Ac turpis egestas sed tempus urna et. Eget egestas purus viverra accumsan in nisl nisi. A lacus vestibulum sed arcu non odio euismod lacinia. Non diam phasellus vestibulum lorem. Tellus id interdum velit laoreet id donec ultrices tincidunt arcu. Dui ut ornare lectus sit amet est placerat in. Lobortis feugiat vivamus at augue eget arcu dictum ius duis. Aliquet eget sit amet tellus cras adipiscing enim eu. Ac auctor augue mauris augue neque gravida in. Quisque sagittis purus sit amet. Etiam erat velit scelerisque in dictum non consectetur. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Ullamcorper eget nulla facilisi etiam dignissim diam. Laoreet id donec ultrices tincidunt arcu.</p>
+            </div>
+        </>
+    )
+}
+
+export default AboutMe;
---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import img_1 from '../images/bg.jpg';
+import img_2 from '../images/man.png';
+import img_3 from '../images/grass.png'
+
+const Home = () => {
+    const height = window.innerHeight;
+    const [offset, setOffset] = useState(0);
+
+    const handleScroll = () => {
+        setOffset(window.scrollY);
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
+
+    return (
+        <>
+            <div className="home">
+                <h1 id="title" style={{ top: (parseInt(height / 2.5) + 0.6 * offset) + "px" }} >Parallax Website</h1>
+                <img src={img_1} id="img_1" />
+                <img src={img_2} style={{ left: 0.9 * offset + "px" }} id="img_2" />
+                <img src={img_3} style={{ left: -0.9 * offset + "px" }} id="img_3" />
+            </div>
+        </>
+    )
+}
+
+export default Home;
